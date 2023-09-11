3936ef86fdb30922afb469d8843df7c620a2b449
ReginoDeang <reginodeangjr@gmail.com>
2023-09-12 05:41:54 +0800

    ntu_pj2_pdt_sales_api

--- src/main/java/com/pdt_sales/pdt_sales/exception/ProductNotFoundException.java
+++ src/main/java/com/pdt_sales/pdt_sales/exception/ProductNotFoundException.java
@@ -1,5 +1,8 @@
 package com.pdt_sales.pdt_sales.exception;
 
-public class ProductNotFoundException {
-    
+public class ProductNotFoundException extends RuntimeException {
+  public ProductNotFoundException(Long id) {
+    super("Could not find customer " + id);
+  }
+
 }
