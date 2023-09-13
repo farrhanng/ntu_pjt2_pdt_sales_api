5210895330f9163be91ea7484b9c7a7ca617e820
BQ <redplatoon@DESKTOP-QA56190>
2023-09-13 20:08:12 +0800

    Additions

--- src/main/java/com/pdt_sales/pdt_sales/controller/ProductController.java
+++ src/main/java/com/pdt_sales/pdt_sales/controller/ProductController.java
@@ -1,5 +1,65 @@
 package com.pdt_sales.pdt_sales.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import lombok.AllArgsConstructor;
+import com.pdt_sales.pdt_sales.entity.Product;
+import com.pdt_sales.pdt_sales.service.ProductService;
+
+@RestController
+@RequestMapping("/products")
+@AllArgsConstructor
 public class ProductController {
-    
+
+    private ProductService productService;
+
+    // CREATE
+    @PostMapping("")
+    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
+        return new ResponseEntity<>(productService.createProduct(product), HttpStatus.CREATED);
+    }
+
+    // READ (GET ALL)
+    @GetMapping("")
+    public ResponseEntity<List<Product>> getAllProducts() {
+        return new ResponseEntity<>(productService.getAllProducts(), HttpStatus.OK);
+    }
+
+    // READ (GET ONE)
+    @GetMapping("{id}")
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        return new ResponseEntity<>(productService.getProduct(id), HttpStatus.OK);
+    }
+
+    // UPDATE
+    @PutMapping("{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return new ResponseEntity<>(productService.updateProduct(id, product), HttpStatus.OK);
+    }
+
+    // DELETE
+    @DeleteMapping("{id}")
+    public ResponseEntity<HttpStatus> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
+    // SEARCH BY SKU
+    @GetMapping("/search")
+    public ResponseEntity<List<Product>> searchProductsBySKU(@RequestParam String sku) {
+        return new ResponseEntity<>(productService.searchProductsBySKU(sku), HttpStatus.OK);
+    }
+
+    // SEARCH BY NAME
+    @GetMapping("/searchByName")
+    public ResponseEntity<List<Product>> searchProductsByName(@RequestParam String name) {
+        return new ResponseEntity<>(productService.searchProductsByName(name), HttpStatus.OK);
+    }
 }
+
--- src/main/java/com/pdt_sales/pdt_sales/repository/ProductRepository.java
+++ src/main/java/com/pdt_sales/pdt_sales/repository/ProductRepository.java
@@ -2,14 +2,6 @@ package com.pdt_sales.pdt_sales.repository;
 
 import com.pdt_sales.pdt_sales.entity.Product;
 
-public class ProductRepository {
-
-    public Product save(Product product) {
-        return null;
-    }
-
-    public Object findById(Long productKey) {
-        return null;
-    }
-    
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByProductSKU(String sku);
 }
