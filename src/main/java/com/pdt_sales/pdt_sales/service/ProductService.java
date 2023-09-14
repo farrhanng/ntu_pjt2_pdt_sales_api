c6489cce29b34a25281f1cf9fb1538d6e6dc2061
farrhanng <work.farrhan@gmail.com>
2023-09-14 15:23:36 +0800

    updated controller and service

--- src/main/java/com/pdt_sales/pdt_sales/service/ProductService.java
+++ src/main/java/com/pdt_sales/pdt_sales/service/ProductService.java
@@ -1,5 +1,24 @@
 package com.pdt_sales.pdt_sales.service;
 
-public class ProductService {
-    
+import java.util.List;
+
+import com.pdt_sales.pdt_sales.entity.Product;
+import com.pdt_sales.pdt_sales.entity.Sales;
+
+public interface ProductService {
+
+  Product createProduct(Product product);
+
+  Product getProduct(Long id);
+
+  List<Product> getAllProducts();
+
+  Product updateProduct(Long id, Product product);
+
+  void deleteProduct(Long id);
+
+  Sales addSalesToProduct(Long id, Sales sales);
+
+  List<Product> searchProducts(String name);
+
 }
--- src/main/java/com/pdt_sales/pdt_sales/service/SalesService.java
+++ src/main/java/com/pdt_sales/pdt_sales/service/SalesService.java
@@ -1,5 +1,19 @@
 package com.pdt_sales.pdt_sales.service;
 
-public class SalesService {
-    
+import java.util.List;
+
+import com.pdt_sales.pdt_sales.entity.Sales;
+
+public interface SalesService {
+
+    Sales saveSales(Sales sales);
+
+    Sales getSales(Long id);
+
+    List<Sales> getAllSales();
+
+    Sales updateSales(Long id, Sales sales);
+
+    void deleteSales(Long id);
+
 }
