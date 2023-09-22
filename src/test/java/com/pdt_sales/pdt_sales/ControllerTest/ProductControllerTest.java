e4dfc0ed06b4fab75ad1df087cd249552d50c92d
root <root@Starlight>
2023-09-22 22:03:28 +0800

    update with some testing code

--- src/test/java/com/pdt_sales/pdt_sales/ControllerTest/ProductControllerTest.java
+++ src/test/java/com/pdt_sales/pdt_sales/ControllerTest/ProductControllerTest.java
@@ -1,5 +1,109 @@
 package com.pdt_sales.pdt_sales.ControllerTest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.pdt_sales.pdt_sales.entity.Product;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import java.util.List;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class ProductControllerTest {
-    
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    public void setUp() {
+        // You can perform any setup needed for your tests here.
+    }
+
+    @DisplayName("Post 1 Product")
+    @Test
+    @WithMockUser(username = "admin", roles = { "ADMIN" })
+    public void createProductTest() throws Exception {
+        // Create a Product object using the builder
+        Product product = Product.builder()
+                .productKey(111L)
+                .productName("Product 1")
+                .modelName("Model 1")
+                .productDescription("Description for Product 1")
+                .productColor("Red")
+                .productSize("Small")
+                .productCost(10.0)
+                .productPrice(20.0)
+                .build();
+
+        // Convert the Product object to JSON
+        String productJson = objectMapper.writeValueAsString(product);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(productJson))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.productKey").value(111L)) // Adjust this assertion based on your response
+                                                                 // structure
+                .andExpect(jsonPath("$.productName").value("Product 1"))
+                .andExpect(jsonPath("$.modelName").value("Model 1"))
+                .andExpect(jsonPath("$.productDescription").value("Description for Product 1"))
+                .andExpect(jsonPath("$.productColor").value("Red"))
+                .andExpect(jsonPath("$.productSize").value("Small"))
+                .andExpect(jsonPath("$.productCost").value(10.0))
+                .andExpect(jsonPath("$.productPrice").value(20.0));
+    }
+
+    @DisplayName("Get All Products")
+    @Test
+    public void getAllProductsTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isArray()); // Assuming your response is a JSON array of products
+    }
+
+    @Test
+    public void getProductByIdTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/products/{productKey}", 1L))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.productKey").value(1)); // Adjust this assertion based on your response structure
+    }
+
+    @Test
+    public void updateProductTest() throws Exception {
+        // Create a Product object with updated data
+        Product updatedProduct = new Product();
+        updatedProduct.setProductName("Updated Product");
+        updatedProduct.setModelName("Updated Model");
+        // Set other fields to update
+
+        // Convert the updated Product object to JSON
+        String updatedProductJson = objectMapper.writeValueAsString(updatedProduct);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/products/{productKey}", 1L)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(updatedProductJson))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.productName").value("Updated Product")); // Adjust this assertion based on your
+                                                                                // response structure
+    }
+
+    @Test
+    public void deleteProductTest() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.delete("/products/{productKey}", 1L))
+                .andExpect(status().isNoContent());
+    }
 }
--- src/test/java/com/pdt_sales/pdt_sales/ServiceImplTest/ProductServiceImplTest.java
+++ src/test/java/com/pdt_sales/pdt_sales/ServiceImplTest/ProductServiceImplTest.java
@@ -1,5 +1,172 @@
 package com.pdt_sales.pdt_sales.ServiceImplTest;
 
+import com.pdt_sales.pdt_sales.entity.Product;
+import com.pdt_sales.pdt_sales.exception.ProductNotFoundException;
+import com.pdt_sales.pdt_sales.repository.ProductRepository;
+import com.pdt_sales.pdt_sales.service.ProductServiceImpl;
+// import com.sun.xml.bind.v2.schemagen.xmlschema.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
 public class ProductServiceImplTest {
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private ProductServiceImpl productService;
+
+    @BeforeEach
+    public void setUp() {
+        // Initialize Mockito annotations
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @DisplayName("Create 1 Product")
+    @Test
+    public void createProductTest() {
+        // Arrange
+        Product productToCreate = Product.builder()
+                .productKey(111L)
+                .productName("Product 1")
+                .modelName("Model 1")
+                .productDescription("Description for Product 1")
+                .productColor("Red")
+                .productSize("Small")
+                .productCost(10.0)
+                .productPrice(20.0)
+                .build();
+
+        when(productRepository.save(productToCreate)).thenReturn(productToCreate);
+
+        // Act
+        Product createdProduct = productService.createProduct(productToCreate);
+
+        // Assert
+        assertEquals(productToCreate, createdProduct, "Created product should match input product");
+        verify(productRepository, times(1)).save(productToCreate);
+    }
+
+    @DisplayName("Get 1 Product by ProductKey")
+    @Test
+    public void getProductTest() {
+        // Arrange
+        Long productKey = 111L;
+        Product product = Product.builder()
+                .productKey(productKey)
+                .productName("Product 1")
+                .modelName("Model 1")
+                .productDescription("Description for Product 1")
+                .productColor("Red")
+                .productSize("Small")
+                .productCost(10.0)
+                .productPrice(20.0)
+                .build();
+
+        when(productRepository.findById(productKey)).thenReturn(Optional.of(product));
+
+        // Act
+        Product retrievedProduct = productService.getProduct(productKey);
+
+        // Assert
+        assertEquals(product, retrievedProduct, "Retrieved product should match expected product");
+    }
+
+    @DisplayName("Get All Products")
+    @Test
+    public void getAllProductsTest() {
+
+        // 1. SETUP
+        // Create a list of product objects and add products directly
+        java.util.List<Product> productList = new ArrayList<>();
+
+        productList.add(Product.builder()
+                .productKey(1L)
+                .productName("Product 1")
+                .modelName("Model 1")
+                .productDescription("Description 1")
+                .productColor("Color 1")
+                .productSize("Size 1")
+                .productCost(10.0)
+                .productPrice(20.0)
+                .build());
+
+        productList.add(Product.builder()
+                .productKey(2L)
+                .productName("Product 2")
+                .modelName("Model 2")
+                .productDescription("Description 2")
+                .productColor("Color 2")
+                .productSize("Size 2")
+                .productCost(15.0)
+                .productPrice(25.0)
+                .build());
+
+        // Mock the findAll method of the product repository to return the productList
+        when(productRepository.findAll()).thenReturn(productList);
+
+        // 2. EXECUTE
+        // Call the method that we want to test
+        java.util.List<Product> retrievedProductList = productService.getAllProducts();
+
+        // 3. ASSERT
+        // Compare the actual result with the expected result
+        assertEquals(productList, retrievedProductList,
+                "The retrieved product list should be the same as the product list");
+    }
+
+
+
+
+
+    // @DisplayName("Update 1 Product")
+    // @Test
+    // public void updateProductTest() {
     
+    //     // 1. SETUP
+    //     // Create a new product object and set a productKey
+    //     Product product = new Product();
+    //     Long productKey = 1L;
+        
+    //     // Mock the findById method of the product repository to return the product object
+    //     when(productRepository.findById(productKey)).thenReturn(Optional.of(product));
+        
+    //     // Mock the save method of the product repository to return the updated product object
+    //     when(productRepository.save(product)).thenReturn(product);
+        
+    //     // 2. EXECUTE
+    //     // Call the method that we want to test
+    //     Product updatedProduct = productService.updateProduct(productKey, product);
+        
+    //     // 3. ASSERT
+    //     // Compare the actual result with the expected result
+    //     assertEquals(product, updatedProduct, "The updated product should be the same as the product object");
+    // }
+
+
+    @DisplayName("Get Product by Non-Existent ProductKey")
+    @Test
+    public void getProductNotFoundTest() {
+        // Arrange
+        Long nonExistentProductKey = 999L;
+        when(productRepository.findById(nonExistentProductKey)).thenReturn(Optional.empty());
+
+        // Act and Assert
+        assertThrows(ProductNotFoundException.class, () -> productService.getProduct(nonExistentProductKey));
+    }
+
 }
